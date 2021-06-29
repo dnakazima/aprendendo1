cdfb3f05405c35ddfd3bde5c647098eaa84cd23e
Dario Nakazima <dnakazima@hotmail.com>
2021-06-29 11:57:58 -0300

    generic2 add outro serviço

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,28 @@
 package application;
 
+import java.util.Scanner;
+
+import services.PrintServiceString;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+
+		PrintServiceString ps = new PrintServiceString();
+
+		System.out.print("How many values? ");
+		int n = sc.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			String value = sc.next();
+			ps.addValue(value);
+		}
+
+		ps.print();
+		System.out.println("First: " + ps.first());
+
+		sc.close();
 	}
-
 }
