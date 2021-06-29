2ed9ade0d115ba31cd6ee34a0225b4bc31b36c71
Dario Nakazima <dnakazima@hotmail.com>
2021-06-29 12:43:39 -0300

    generic3 utilizando Object

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,7 +2,9 @@ package application;
 
 import java.util.Scanner;
 
-import services.PrintServiceString;
+import services.PrintService;
+
+
 
 public class Program {
 
@@ -10,16 +12,20 @@ public class Program {
 
 		Scanner sc = new Scanner(System.in);
 
-		PrintServiceString ps = new PrintServiceString();
+		PrintService ps = new PrintService();
 
 		System.out.print("How many values? ");
 		int n = sc.nextInt();
-
+		
+		//aceita inserir string problema de segurança
+		//ps.addValue("Maria") por exemplo
+		
 		for (int i = 0; i < n; i++) {
 			String value = sc.next();
 			ps.addValue(value);
 		}
-
+		//nao pode integer para object
+		
 		ps.print();
 		System.out.println("First: " + ps.first());
 
