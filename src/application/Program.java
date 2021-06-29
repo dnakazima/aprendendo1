33befc392fb2288d427356180c7e371481adbacd
Dario Nakazima <dnakazima@hotmail.com>
2021-06-29 15:41:08 -0300

    generic3 utilizando com generics

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,27 +4,22 @@ import java.util.Scanner;
 
 import services.PrintService;
 
-
-
 public class Program {
 
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 
-		PrintService ps = new PrintService();
+		PrintService<Integer> ps = new PrintService<Integer>();
 
 		System.out.print("How many values? ");
 		int n = sc.nextInt();
-		
-		//aceita inserir string problema de segurança
-		//ps.addValue("Maria") por exemplo
+				
 		
 		for (int i = 0; i < n; i++) {
-			String value = sc.next();
+			Integer value = sc.nextInt();
 			ps.addValue(value);
 		}
-		//nao pode integer para object
 		
 		ps.print();
 		System.out.println("First: " + ps.first());
